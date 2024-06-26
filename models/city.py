e9b7e72e00828e21cd9ca053d2034049d79c72ac
Amany Mohammed <amany.mohammd@gmail.com>
2024-06-27 02:39:35 +0300

    Add models and their tests

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ city class contains state ID and name """
+    __tablename__ = 'cities'
+
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+        state_id = ""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            '''
+                Return list of city instances if City.state_id==current
+                State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            list_cities = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
