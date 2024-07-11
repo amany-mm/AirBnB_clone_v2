31efbea6020958e953abc8764112bee8e499d9fc
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-12 00:03:25 +0300

    Add Fabric script that distributes an archive to your web servers, using the function do_deploy.

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy.
+
+Usage:
+    fab -f 2-do_deploy_web_static.py
+    do_deploy:archive_path=versions/web_static_20240711003959.tgz
+    -i my_ssh_private_key -u ubuntu
+"""
+from fabric.api import env, put, run
+from os import path
+
+env.hosts = ['52.207.208.230', '18.204.11.104']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+
+
+def do_deploy(archive_path):
+    """Deploy web files to server
+    """
+    try:
+        if not (path.exists(archive_path)):
+            return False
+
+        put(archive_path, '/tmp/')
+
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+        run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+            .format(timestamp))
+
+        run('sudo rm -rf /data/web_static/current')
+
+        run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+    except:
+        return False
+
+    return True
